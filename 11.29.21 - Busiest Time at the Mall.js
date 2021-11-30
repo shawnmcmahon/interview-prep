66596445891200ca34ba9797d09c715019fc97e9
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-11-29 23:13:00 -0700

    Add more logic

--- 11.29.21 - Busiest Time at the Mall.js
+++ 11.29.21 - Busiest Time at the Mall.js
@@ -1,58 +1,4 @@
-// Busiest Time in The Mall
-// The Westfield Mall management is trying to figure out what the busiest moment at the mall was last year. You’re given data extracted from the mall’s door detectors. Each data point is represented as an integer array whose size is 3. The values at indices 0, 1 and 2 are the timestamp, the count of visitors, and whether the visitors entered or exited the mall (0 for exit and 1 for entrance), respectively. Here’s an example of a data point: [ 1440084737, 4, 0 ].
-
-// Note that time is given in a Unix format called Epoch, which is a nonnegative integer holding the number of seconds that have elapsed since 00:00:00 UTC, Thursday, 1 January 1970.
-
-// Given an array, data, of data points, write a function findBusiestPeriod that returns the time at which the mall reached its busiest moment last year. The return value is the timestamp, e.g. 1480640292. Note that if there is more than one period with the same visitor peak, return the earliest one.
-
-// Assume that the array data is sorted in an ascending order by the timestamp. Explain your solution and analyze its time and space complexities.
-
-// Example:
-
-// input:  data = [ [1487799425, 14, 1], 
-//                  [1487799425, 4,  0],
-//                  [1487799425, 2,  0],
-//                  [1487800378, 10, 1],
-//                  [1487801478, 18, 0],
-//                  [1487801478, 18, 1],
-//                  [1487901013, 1,  0],
-//                  [1487901211, 7,  1],
-//                  [1487901211, 7,  0] ]
-
-// output: 1487800378 # since the increase in the number of people
-//                    # in the mall is the highest at that point
-// Constraints:
-
-// [time limit] 5000ms
-
-// [input] array.array.integer data
-
-// 1 ≤ data.length ≤ 100
-// [output] integer
-
-  // your code goes here
-  // Reduce through the data array 
-  // Flatten the current index or look inside each index of that array or splice it and look at that value
-  // assign index zero to time 
-    // index two to amount of people 
-    // and index three to inOrOut
-  
-  // assign a highRangTime variable at 1487799420 
-    // lowRangeTime variable at 1487794420
-  // assign variable called topTrafficTime to 0
-    // assign variable called currentTrafficPop to 0
-    // assign variable called topTrafficPop
-  // Look at currentIndex[0]
-    // if value is between low or highRangTime variables 
-      // and currentIndex[1] equal 1
-         // then add the amount to currentTrafficPop 
-      // if currentIndex[0] equal 0 
-          // subtract from currentPop 
-  // if currentTrafficPop is > topTrafficPop 
-    // then save currentIndex[0] to topTrafficTime 
-  // return topTrafficTime
-
-  function findBusiestPeriod(data) {
+function findBusiestPeriod(data) {
     // your code goes here
     // Reduce through the data array 
     // Flatten the current index or look inside each index of that array or splice it and look at that value
@@ -60,8 +6,7 @@
       // index two to amount of people 
       // and index three to inOrOut
     
-    // assign a highRangTime variable at 1487799420 
-      // lowRangeTime variable at 1487794420
+    // assign a variable called currentTime
     // assign variable called topTrafficTime to 0
       // assign variable called currentTrafficPop to 0
       // assign variable called topTrafficPop
@@ -74,64 +19,67 @@
     // if currentTrafficPop is > topTrafficPop 
       // then save currentIndex[0] to topTrafficTime 
     // return topTrafficTime
-    function findBusiestPeriod(data) {
-        // your code goes here
-        // Reduce through the data array 
-        // Flatten the current index or look inside each index of that array or splice it and look at that value
-        // assign index zero to time 
-          // index two to amount of people 
-          // and index three to inOrOut
-        
-        // assign a variable called currentTime
-        // assign variable called topTrafficTime to 0
-          // assign variable called currentTrafficPop to 0
-          // assign variable called topTrafficPop
-        // Look at currentIndex[0]
-          // if value is between low or highRangTime variables 
-            // and currentIndex[1] equal 1
-               // then add the amount to currentTrafficPop 
-            // if currentIndex[0] equal 0 
-                // subtract from currentPop 
-        // if currentTrafficPop is > topTrafficPop 
-          // then save currentIndex[0] to topTrafficTime 
-        // return topTrafficTime
-        
-        let currentTime = 14877999420;
-        let currentTrafficPop = 0; 
-        let topTrafficPop = 0; 
-        
-        
-        const busiestTime = data.reduce((topTrafficTime, currentIndex) => {
-          console.log("hi")
+    let timeInterval = [1487799425, 1487901211];
+    let currentTime = 1487799420;
+    let currentTrafficPop = 0; 
+    let topTrafficPop = 0; 
+    
+  
+  let time = timeInterval.map(currentTimeInterval => {
+        currentTime = currentTimeInterval; 
+        let busiestTime = data.reduce((topTrafficTime, currentIndex) => {
+          console.log(currentTime)
+          console.log("start of", currentIndex[0], "at", currentTrafficPop)
           if (currentIndex[0] >= currentTime && currentIndex[2] === 1) {
-             currentTrafficPop += currentIndex[1];
-          } else if (currentIndex[0] >= currentTime && currentIndex[2] === 0) {
+            console.log(currentTrafficPop)
+            currentTrafficPop += currentIndex[1];
+            if (currentTrafficPop > topTrafficPop) {
+              topTrafficPop = currentTrafficPop
+            }
+          } 
+          
+          if (currentIndex[0] >= currentTime && currentIndex[2] === 0) {
+            //console.log(currentTrafficPop)
             currentTrafficPop -= currentIndex[1];
+            if (currentTrafficPop > topTrafficPop) {
+              topTrafficPop = currentTrafficPop
+            }
           };
-       
+          //console.log(currentTrafficPop)
+      
           if (currentTrafficPop > topTrafficPop) {
+            //console.log(currentTrafficPop, "during its top")
             topTrafficTime = currentIndex[0];
           };
           
-          currentTime++;
-          
-          return topTrafficTime;
-        }, 0)
         
-        return busiestTime; 
-      }
-
-    }
-      
-      let data = [ [1487799425, 14, 1], 
-                       [1487799425, 4,  0],
-                       [1487799425, 2,  0],
-                       [1487800378, 10, 1],
-                       [1487801478, 18, 0],
-                       [1487801478, 18, 1],
-                       [1487901013, 1,  0],
-                       [1487901211, 7,  1],
-                       [1487901211, 7,  0] ]
-      
-      
-      findBusiestPeriod(data)
+              currentTime++
+  
+              return topTrafficTime;
+        }, 0)
+  
+      console.log(`the busiest tiem is ${busiestTime} at ${topTrafficPop}`)
+        return busiestTime;
+  
+  })
+  
+  
+  }
+  
+  
+  
+  
+        
+  
+  let data = [ [1487799425, 14, 1], 
+                   [1487799425, 4,  0],
+                   [1487799425, 2,  0],
+                   [1487800378, 10, 1],
+                   [1487801478, 18, 0],
+                   [1487801478, 18, 1],
+                   [1487901013, 1,  0],
+                   [1487901211, 7,  1],
+                   [1487901211, 7,  0] ]
+  
+  
+  findBusiestPeriod(data)

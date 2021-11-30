48e74564b075f1a789386059f5b5b5714b502030
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-11-29 22:17:12 -0700

    Add rough draft first answer

--- 11.29.21 - Busiest Time at the Mall.js
+++ 11.29.21 - Busiest Time at the Mall.js
@@ -30,3 +30,108 @@
 // 1 ≤ data.length ≤ 100
 // [output] integer
 
+  // your code goes here
+  // Reduce through the data array 
+  // Flatten the current index or look inside each index of that array or splice it and look at that value
+  // assign index zero to time 
+    // index two to amount of people 
+    // and index three to inOrOut
+  
+  // assign a highRangTime variable at 1487799420 
+    // lowRangeTime variable at 1487794420
+  // assign variable called topTrafficTime to 0
+    // assign variable called currentTrafficPop to 0
+    // assign variable called topTrafficPop
+  // Look at currentIndex[0]
+    // if value is between low or highRangTime variables 
+      // and currentIndex[1] equal 1
+         // then add the amount to currentTrafficPop 
+      // if currentIndex[0] equal 0 
+          // subtract from currentPop 
+  // if currentTrafficPop is > topTrafficPop 
+    // then save currentIndex[0] to topTrafficTime 
+  // return topTrafficTime
+
+  function findBusiestPeriod(data) {
+    // your code goes here
+    // Reduce through the data array 
+    // Flatten the current index or look inside each index of that array or splice it and look at that value
+    // assign index zero to time 
+      // index two to amount of people 
+      // and index three to inOrOut
+    
+    // assign a highRangTime variable at 1487799420 
+      // lowRangeTime variable at 1487794420
+    // assign variable called topTrafficTime to 0
+      // assign variable called currentTrafficPop to 0
+      // assign variable called topTrafficPop
+    // Look at currentIndex[0]
+      // if value is between low or highRangTime variables 
+        // and currentIndex[1] equal 1
+           // then add the amount to currentTrafficPop 
+        // if currentIndex[0] equal 0 
+            // subtract from currentPop 
+    // if currentTrafficPop is > topTrafficPop 
+      // then save currentIndex[0] to topTrafficTime 
+    // return topTrafficTime
+    function findBusiestPeriod(data) {
+        // your code goes here
+        // Reduce through the data array 
+        // Flatten the current index or look inside each index of that array or splice it and look at that value
+        // assign index zero to time 
+          // index two to amount of people 
+          // and index three to inOrOut
+        
+        // assign a variable called currentTime
+        // assign variable called topTrafficTime to 0
+          // assign variable called currentTrafficPop to 0
+          // assign variable called topTrafficPop
+        // Look at currentIndex[0]
+          // if value is between low or highRangTime variables 
+            // and currentIndex[1] equal 1
+               // then add the amount to currentTrafficPop 
+            // if currentIndex[0] equal 0 
+                // subtract from currentPop 
+        // if currentTrafficPop is > topTrafficPop 
+          // then save currentIndex[0] to topTrafficTime 
+        // return topTrafficTime
+        
+        let currentTime = 14877999420;
+        let currentTrafficPop = 0; 
+        let topTrafficPop = 0; 
+        
+        
+        const busiestTime = data.reduce((topTrafficTime, currentIndex) => {
+          console.log("hi")
+          if (currentIndex[0] >= currentTime && currentIndex[2] === 1) {
+             currentTrafficPop += currentIndex[1];
+          } else if (currentIndex[0] >= currentTime && currentIndex[2] === 0) {
+            currentTrafficPop -= currentIndex[1];
+          };
+       
+          if (currentTrafficPop > topTrafficPop) {
+            topTrafficTime = currentIndex[0];
+          };
+          
+          currentTime++;
+          
+          return topTrafficTime;
+        }, 0)
+        
+        return busiestTime; 
+      }
+
+    }
+      
+      let data = [ [1487799425, 14, 1], 
+                       [1487799425, 4,  0],
+                       [1487799425, 2,  0],
+                       [1487800378, 10, 1],
+                       [1487801478, 18, 0],
+                       [1487801478, 18, 1],
+                       [1487901013, 1,  0],
+                       [1487901211, 7,  1],
+                       [1487901211, 7,  0] ]
+      
+      
+      findBusiestPeriod(data)
